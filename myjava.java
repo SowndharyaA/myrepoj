0164d78adc34036a896682d825554bbad49fb58b
SowndharyaA <111833185+SowndharyaA@users.noreply.github.com>
2022-08-23 14:48:53 +0530

    Add files via upload

---
+++ myjava.java
@@ -0,0 +1,19 @@
+
+public class MyClass {
+	
+	public void myMethod() {
+		System.out.println("This is myMethod  ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is main method ");
+		
+		MyClass m = new MyClass();
+		m.myMethod();
+		
+
+	}
+
+}
